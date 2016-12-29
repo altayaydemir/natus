ba324ee49f76bd88505d36623288839c6d186a29
Altay Aydemir <altay.aydemir@gmail.com>
2016-12-29 19:56:09 +0300

    Add env specific client_id and redirect_uri

--- config/index.js
+++ config/index.js
@@ -1,15 +1,17 @@
 const path = require('path');
 
-module.exports = {
+const env = process.env.NODE_ENV || 'development';
+
+const config = {
   path: path.resolve(__dirname, '../config'),
   input: path.resolve(__dirname, '../src'),
   output: path.resolve(__dirname, '../dist'),
   port: process.env.PORT || 8000,
   host: process.env.HOST || 'localhost',
-  env: process.env.NODE_ENV || 'development',
+  env,
   apiURL: 'https://api.put.io/v2',
-  // client_id: 2694,
-  // redirect_uri: 'http://localhost:8000/auth',
-  client_id: 2691,
-  redirect_uri: 'http://natus.zipzip.top/auth',
+  client_id: env === 'development' ? 2694 : 2691,
+  redirect_uri: env === 'development' ? 'http://localhost:8000/auth' : 'http://natus.zipzip.top/auth',
 };
+
+module.exports = config;
